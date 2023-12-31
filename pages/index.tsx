3f0376d31c1c80a569b0264cb0eb6802ab1ed444
hadyanaa <hadyanabdulaziz@gmail.com>
2023-12-31 15:44:15 +0700

    :sparkles: create section about

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,6 +3,7 @@ import type { GetServerSideProps } from 'next';
 import { serverSideTranslations } from 'next-i18next/serverSideTranslations';
 import { useTranslation } from 'next-i18next';
 import Header from '@/components/layout/header';
+import Image from 'next/image';
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -12,10 +13,31 @@ export default function Home() {
   return (
     <>
       <Header />
+      {/* jumbotron */}
       <div className="mt-8 flex flex-col items-center justify-center">
-        <img className="w-20 h-20 rounded-full" src="/image/hadyan.jpg" alt="Foto Hadyan" />
+        <Image
+          className="rounded-full"
+          src={"/image/hadyan.jpg"}
+          width={120}
+          height={120}
+          alt='Foto Hadyan'
+        />
         <h1 className="text-lg">{t('identity.name')}</h1>
         <p>{t('identity.work')}</p>
+      </div>
+      {/* about */}
+      <div className="mt-8 flex flex-col gap-4">
+        <div className="flex items-center justify-center">
+          <h2>About Me</h2>
+        </div>
+        <div className="mx-auto grid grid-cols-2 gap-8 w-[60%]">
+          <div className="flex justify-center items-center text-center">
+            <p>Saya adalah seorang fresh graduate jurusan Sistem Informasi yang berkuliah di Sekolah Tinggi Teknologi Terpadu <a href="https://nurulfikri.ac.id/" target="_blank">Nurul Fikri</a>.</p>
+          </div>
+          <div className="flex justify-center items-center text-center">
+            <p>Aktivitas saat ini adalah menjadi asisten dosen mata kuliah basis data dan junior frontend developer. Untuk profile lebih lanjut bisa cek di <a href="https://www.linkedin.com/in/hadyanaa/" target="_blank">Linkedin</a> saya.</p>
+          </div>
+        </div>
       </div>
     </>
   )
